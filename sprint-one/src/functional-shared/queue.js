db14f3a08d1ee62e616bbea70a0d26b594ed10da
asponring <asponring@gmail.com>
2015-03-25 15:52:43 -0700

    Implemented queue and stack with functional shared pattern

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,30 @@
 var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance={};
+  someInstance.storage = {};
+  someInstance.entrance = 0;
+  someInstance.exit = 0;
+
+  return _.extend(someInstance, queueMethods);
 };
 
-var queueMethods = {};
+var queueMethods = {
+  size : function(){
+    return this.entrance - this.exit;
+  },
+
+  enqueue : function(val){
+    this.storage[this.entrance++] = val;
+  },
+
+  dequeue : function (){
+    if(this.exit < this.entrance){
+      return this.storage[this.exit++];
+    }
+    return;
+  }
+};
 
 
 
