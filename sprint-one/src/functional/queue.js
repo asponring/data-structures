6187615d80a671a74a211b97c01e9595d72e539d
asponring <asponring@gmail.com>
2015-03-25 15:08:57 -0700

    Did first queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,24 @@ var Queue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var entrance = 0;
+  var exit = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[entrance++] = value;
   };
 
   someInstance.dequeue = function(){
+    if (exit < entrance) {
+      return storage[exit++];
+    }
+    return;
   };
 
   someInstance.size = function(){
+    return entrance - exit;
   };
 
   return someInstance;
