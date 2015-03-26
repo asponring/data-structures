d06a7da4ddf2e06cf831efa686a4dc67bfcbc130
asponring <asponring@gmail.com>
2015-03-25 19:41:30 -0700

    Resolved pseudoclassical but one test fails...bad test?

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,24 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.entrance = 0;
+  this.exit = 0;
+};
+
+Queue.prototype.enqueue = function(val){
+  this.storage[this.entrance++] = val;
+};
+
+Queue.prototype.dequeue = function(){
+  if(this.entrance > this.exit){
+    return this.storage[this.exit++];
+  }
+  return;
+};
+
+Queue.prototype.size = function(){
+  return this.entrance - this.exit;
 };
 
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,24 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.numItems = 0;
 };
+
+Stack.prototype.push = function(val){
+  this.storage[this.numItems++] = val;
+};
+
+Stack.prototype.pop = function(){
+  if(this.numItems > 0){
+    return this.storage[--this.numItems];
+  }
+  return;
+};
+
+Stack.prototype.size = function(){
+  return this.numItems;
+};
+
 
 
