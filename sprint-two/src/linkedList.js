8773b9e32144fbd58d9e8e752d4e3a0987c7fc9e
asponring <asponring@gmail.com>
2015-03-26 10:38:28 -0700

    Implemented linked lists, passes all tests for linked lists

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,37 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var newNode = Node(value);
+    if (this.tail !== null) {
+      this.tail.next = newNode;
+    }
+    if (this.head === null) {
+      this.head = newNode;
+    }
+    this.tail = newNode;
   };
 
   list.removeHead = function(){
+    if (this.head === null) {
+      return null;
+    }
+    var result = this.head.value;
+    this.head = this.head.next;
+    return result;
   };
 
   list.contains = function(target){
+    var f = function(node){
+      if(node.value === target){
+        return true;
+      }
+      if(node.next === null){
+        return false;
+      }
+      return f(node.next);
+    }
+
+    return f(this.head);
   };
 
   return list;
@@ -26,4 +51,7 @@ var Node = function(value){
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * contains: O(n)
+ * removeHead: O(1)
+ * addTail: O(1)
  */
