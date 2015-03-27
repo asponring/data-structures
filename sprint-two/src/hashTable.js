0959a119ceaa3d7013310d0394beb94e93d16a5f
asponring <asponring@gmail.com>
2015-03-27 12:26:27 -0700

    Completed hash tables, passed all tests

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,19 +5,39 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var tuple = [k, v];
+  if(!this._storage.get(i)){
+    this._storage.set(i,[]);
+  }
+  var bucket = this._storage.get(i);
+  bucket.push(tuple);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  var bucket = this._storage.get(i);
+  var result = _.find(bucket, function(elem, i) {
+    return elem[0] === k;
+  });
+  console.log("result is: ", result);
+  return (result === undefined) ? null :result[1];
 };
 
 HashTable.prototype.remove = function(k){
-
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(i);
+  var newBucket = _.reject(bucket, function(elem, i){
+    return elem[0] === k;
+  });
+  this._storage.set(i, newBucket);
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * insert: O(1)
+ * retrieve: O(1)
+ * remove: O(1)
+ * Assuming that the buckets don't get too big...
  */
