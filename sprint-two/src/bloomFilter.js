301cdb93f5205e028913056977403e7c6841dd35
asponring <asponring@gmail.com>
2015-03-27 16:41:39 -0700

    Began bloomFilter

---
+++ sprint-two/src/bloomFilter.js
@@ -0,0 +1,13 @@
+var BloomFilter = function(size) {
+
+};
+
+BloomFilter.prototype.hashFunctions = [];
+
+BloomFilter.prototype.add = function() {
+
+};
+
+BloomFilter.prototype.contains = function(val)
+
+var bloomFilter = new BloomFilter(18);
